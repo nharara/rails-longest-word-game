86a0a67df28ab63634d0c780d5d6591c8af4c343
Nozomu Hara <slipper-spells-0j@icloud.com>
2023-09-19 17:30:26 +0900

    Fixedvalid method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,36 +1,42 @@
 require "open-uri"
 
 class GamesController < ApplicationController
-  def new
-    @letters = generate_random_letters(10)
-  end
+  VOWELS = %w(a e i o u y)
 
-  def generate_random_letters(length)
-    letters = ('a'..'z').to_a
-    Array.new(length) { letters.sample }
+  def new
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('a'..'z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
+  # def new
+  #   @letters = generate_random_letters(10)
+  # end
+
+  # def generate_random_letters(length)
+  #   letters = ('a'..'z').to_a
+  #   Array.new(length) { letters.sample }
+  # end
 
   def score
     @saved_letters = params[:letters].split('')
     @user_guess = params[:query]
 
-    @valid = @user_guess.split('').all? { |letter| @saved_letters.count(letter) >= @user_guess.count(letter) }
-
-    # if @vali
-    #   @message = "Can make that word"
-    # else
-    #   @message = "Cannot make that word out of the letters"
-    # end
     url = "https://wagon-dictionary.herokuapp.com/#{@user_guess}"
     read_url = URI.open(url).read
     @okay = JSON.parse(read_url)
 
-    if @valid && @okay['found'] == true
-      @result = "Congratulations! '#{@user_guess}.capitalize' can be made out of these letters"
-    elsif @valid && @okay['found'] == false
-      @result = "#{@user_guess} is a word but can't be made out of these letters."
+    if valid?
+      if @okay['found'] == true
+        @result = "Congratulations! '#{@user_guess}' can be made out of these letters 🙏"
+      else
+        @result = "#{@user_guess} is not an English word 😅"
+      end
     else
-      @result = "#{@user_guess} is not a word"
+      @result = "#{@user_guess} can't be made out of these letters 😔"
     end
+  end
+
+  def valid?
+    @user_guess.split('').all? { |letter| @saved_letters.count(letter) >= @user_guess.count(letter) }
   end
 end
